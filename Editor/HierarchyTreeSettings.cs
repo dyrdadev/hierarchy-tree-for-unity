42cb279f1cc3cbca89dd4fd574ea6b9a70aba6bf
Jonas Wischeropp <jonas@wischeropp.de>
2024-07-14 14:52:16 +0200

    fixed indentation style

--- Editor/HierarchyTreeSettings.cs
+++ Editor/HierarchyTreeSettings.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 namespace DyrdaDev.ForUnity.Hierarchy
 {
     [FilePath("Hierarchy/HierarchyTree/Settings.asset", FilePathAttribute.Location.PreferencesFolder)]
-    internal class HierarchyTreeSettings : ScriptableSingleton<HierarchyTreeSettings> {
+    internal class HierarchyTreeSettings : ScriptableSingleton<HierarchyTreeSettings>
+    {
         [field: SerializeField]
         public bool Enabled {get; private set;} = true;
 
@@ -17,10 +18,12 @@ namespace DyrdaDev.ForUnity.Hierarchy
         public Color HighlightedEdgeColor {get; private set;} = new Color(0.49f, 0.678f, 0.952f);
 
         [SettingsProvider]
-        public static SettingsProvider CreateHierarchyTreeSettingsProvider() {
+        public static SettingsProvider CreateHierarchyTreeSettingsProvider()
+        {
             var keywords = new string[]{"Hierarchy", "Tree", "Edge Width", "Edge Color", "Highlighted Edge Color"};
 
-            return new SettingsProvider("Preferences/Hierarchy/Hierarchy Tree", SettingsScope.User, keywords) {
+            return new SettingsProvider("Preferences/Hierarchy/Hierarchy Tree", SettingsScope.User, keywords)
+            {
                 guiHandler = (searchContext) => {
                     instance.SetEnabled(EditorGUILayout.Toggle("Active", instance.Enabled));
                     instance.SetEdgeWidth(EditorGUILayout.Slider("Edge Width", instance.EdgeWidth, 1f, 6f));
@@ -31,30 +34,37 @@ namespace DyrdaDev.ForUnity.Hierarchy
             };
         }
         
-        private void SaveWhenDifferent<T>(T oldValue, T newValue) {
+        private void SaveWhenDifferent<T>(T oldValue, T newValue)
+        {
             if (!oldValue.Equals(newValue))
+            {
                 Save(true);
+            }
         }
 
-        private void SetEnabled(bool enabled) {
+        private void SetEnabled(bool enabled)
+        {
             var old = Enabled;
             Enabled = enabled;
             SaveWhenDifferent(old, Enabled);
         }
 
-        private void SetEdgeWidth(float edgeWidth) {
+        private void SetEdgeWidth(float edgeWidth)
+        {
             var old = EdgeWidth;
             EdgeWidth = edgeWidth;
             SaveWhenDifferent(old, EdgeWidth);
         }
 
-        private void SetEdgeColor(Color edgeColor) {
+        private void SetEdgeColor(Color edgeColor)
+        {
             var old = EdgeColor;
             EdgeColor = edgeColor;
             SaveWhenDifferent(old, EdgeColor);
         }
 
-        private void SetHighlightEdgeColor(Color highlightedEdgeColor) {
+        private void SetHighlightEdgeColor(Color highlightedEdgeColor)
+        {
             var old = HighlightedEdgeColor;
             HighlightedEdgeColor = highlightedEdgeColor;
             SaveWhenDifferent(old, HighlightedEdgeColor);
